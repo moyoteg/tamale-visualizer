34ea6134fe92def1ee15d3396bf95697fb9e0bd6
Moi Gutierrez <moyoteg@gmail.com>
2019-10-23 00:47:44 -0500

    added providers viewer

---
+++ src/views/ProvidersViewer.js
@@ -0,0 +1,55 @@
+import React, {
+} from 'react'
+import CollectionListVisualizer from '../views/CollectionListVisualizer'
+import ProviderVisualizer from '../views/ProviderVisualizer'
+import FakerDataProvider from '../Helpers/DataProviders/FakerDataProvider'
+import FirebaseDataProvider from '../Helpers/DataProviders/FirebaseDataProvider'
+import strings from '../Helpers/Strings'
+
+export default function ProvidersViewer() {
+
+    const filterCollectionByFunction = (provider, searchString, filterBy) => {
+      if (provider && searchString) {
+        // filter by:...
+        console.log("filter by: " + filterBy)
+        switch (filterBy) {
+          default:
+            return provider.name.toLowerCase().includes(searchString.toLowerCase()) ||
+              provider.description.toLowerCase().includes(searchString.toLowerCase())
+        }
+      } else {
+        return true
+      }
+    }
+  
+    const getCollectionDataFunction = async (useMockData = false) => {
+      if (useMockData) {
+        // from local json
+        // setProviders(sampleProviderData)
+        // from Faker.js
+        return await FakerDataProvider.getProviders(100)
+      } else {
+        return await FirebaseDataProvider.getProviders()
+      }
+    }
+  
+    // TODO: figure out why using an array like this does not give strings
+    const filterByOptions = null
+  
+    const collectionName = strings.provider
+  
+    const visualizer = (provider) => {
+      return (<ProviderVisualizer provider={provider} ></ProviderVisualizer>)
+    }
+  
+    return (
+      <CollectionListVisualizer
+        collectionName={collectionName}
+        filterCollectionByFunction={filterCollectionByFunction}
+        filterByOptionsProp={filterByOptions}
+        getCollectionDataFunction={getCollectionDataFunction}
+        useMockData={true}
+        visualizer={visualizer}
+      />
+    )
+  }
